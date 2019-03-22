e3d459e0718e42ec9645f87c1ed15cbb41889fc6
Quan Le <quanle@Quans-MacBook-Pro-2.local>
2019-03-22 11:39:03 -0700

    added mysql library, config, and gitignore files

--- server/server.js
+++ server/server.js
@@ -1,6 +1,9 @@
 const express = require('express'); // load express library into file
-
+const mysql = require('mysql');
 const server = express();
+const mysquelcredentials = require('./mysqlcreds.js');
+
+const db = mysql.createConnection(mysquelcredentials);
 
 server.use(express.static( __dirname + '/html'))
 
